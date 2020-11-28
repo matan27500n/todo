c3145509c289ed648174f3f18012332d8046df7c
matan27500n <matan27500n@gmail.com>
2020-11-28 09:47:39 +0200

    creating components and services

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListTodoComponent } from './components/list-todo/list-todo.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    FooterComponent,
+    ListTodoComponent,
+    LoginComponent,
+    LogoutComponent,
+    MenuComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
